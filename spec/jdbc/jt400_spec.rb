2096938877be0364773fd8158cabb996c8eadd3e
Per Lundberg <per.lundberg@ecraft.com>
2017-02-01 15:34:24 +0200

    Added more commentary.

--- spec/jdbc/jt400_spec.rb
+++ spec/jdbc/jt400_spec.rb
@@ -2,7 +2,8 @@ module Jdbc
   describe JT400 do
     subject { described_class }
 
-    # Note: this test needs a live DB2 server.
+    # Note: this test needs a live DB2 server. Pass in a valid as400 connection string in the `DB2_URI` env variable and you're
+    # all set.
     describe '.load_driver' do
       it 'loads the driver, making it accessible in the JDBC DriverManager' do
         described_class.load_driver
